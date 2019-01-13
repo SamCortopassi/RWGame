596b733e359a864c2b2b3d4ce3f903a9f81a2ecf
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-13 16:45:27 -0600

    connect button

--- RWGame/ViewController.swift
+++ RWGame/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func showAllert() {
+        print("Hello!")
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
