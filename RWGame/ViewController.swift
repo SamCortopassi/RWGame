9cb7605558c9d12b553e836891980cb5805318ea
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-13 16:59:13 -0600

    popup

--- RWGame/ViewController.swift
+++ RWGame/ViewController.swift
@@ -15,8 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func showAllert() {
-        print("Hello!")
+    @IBAction func showAlert() {
+
+    let alert = UIAlertController(title: "Hello World", message: "this is my first app!", preferredStyle: .alert)
+        
+    let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
