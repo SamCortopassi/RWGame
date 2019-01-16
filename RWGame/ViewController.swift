1fa5c40723cd3b992aa416d5480210ac628918da
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-16 17:16:34 -0600

    new round delay

--- RWGame/ViewController.swift
+++ RWGame/ViewController.swift
@@ -30,32 +30,39 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert() {
         let difference = abs(targetValue - currentValue)
-        let points = 100 - difference
+        var points = 100 - difference
+
+       score += points
         
-        score += points
-        
-//        if currentValue > targetValue {
-//            difference = currentValue - targetValue
-//        } else if targetValue > currentValue {
-//            difference = targetValue - currentValue
-//        } else {
-//            difference = 0
-//        }
-        
-//        let message = "the value of the slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)" + "\n the difference is: \(difference)"
-//
+    let title: String
+        if difference == 0 {
+            title = "Perfect"
+            points += 100
+        } else if difference < 5 {
+            title = "So close"
+            if difference == 1 {
+                points += 1
+            }
+        } else if difference < 10 {
+            title = "Pretty good"
+        } else {
+            title = "Not even close..."
+        }
         
     let message = "Your score is \(points)"
         
-    let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-    let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+    let action = UIAlertAction(title: "OK", style: .default, handler: {
+        action in
+        self.startNewRound()
+        
+    })
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
         
-        startNewRound()
     }
 
     @IBAction func sliderMoved(_ slider: UISlider) {
