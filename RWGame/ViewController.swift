dfeb7561b9bf474882648ffb0c12133a8f185d54
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-16 16:47:05 -0600

    round label

--- RWGame/ViewController.swift
+++ RWGame/ViewController.swift
@@ -13,10 +13,12 @@ class ViewController: UIViewController {
     var currentValue = 0
     var targetValue = 0
     var score = 0
+    var round = 0
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,10 +63,8 @@ class ViewController: UIViewController {
         currentValue = Int(slider.value)
     }
     
-    
-    
-    
     func startNewRound() {
+        round += 1
         targetValue = Int(arc4random_uniform(100))+1
         currentValue = 50
         slider.value = Float(currentValue)
@@ -75,6 +75,7 @@ class ViewController: UIViewController {
     func updateLabels() {
         targetLabel.text = String(targetValue)
         scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 
 }
