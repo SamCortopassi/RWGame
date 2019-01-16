b1e68d87d984856d4ea9d5a99fbc9cabf1540452
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-16 12:35:20 -0600

    read start value

--- RWGame/ViewController.swift
+++ RWGame/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
 
     var currentValue: Int = 0
     
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(slider.value)
     }
     
     @IBAction func showAlert() {
