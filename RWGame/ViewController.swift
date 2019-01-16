25e0371e79e0a63c274766e13918916e6a5e429e
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-16 13:11:28 -0600

    random broken

--- RWGame/ViewController.swift
+++ RWGame/ViewController.swift
@@ -11,17 +11,19 @@ import UIKit
 class ViewController: UIViewController {
 
     var currentValue: Int = 0
+    var targetValue: Int = 0
     
     @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let roundedValue = slider.value.rounded()
-        currentValue = Int(slider.value)
+        currentValue = Int(roundedValue)
+        targetValue = Int.random(in: 1...100)
     }
     
     @IBAction func showAlert() {
-    let message = "the value of the slider is now: \(currentValue)"
+        let message = "the value of the slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
     let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
         
