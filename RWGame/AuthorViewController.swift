048886892c15fbff399a070a1f5e2ae8fb327df0
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-16 18:58:26 -0600

    multi page

---
+++ RWGame/AuthorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AuthorViewController.swift
+//  RWGame
+//
+//  Created by Samantha Cortopassi on 1/16/19.
+//  Copyright © 2019 Samantha Cortopassi. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
