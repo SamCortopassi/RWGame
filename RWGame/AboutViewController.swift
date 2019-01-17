b252f21fa29c544971602b4fccb57e949126e1f8
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-16 18:30:55 -0600

    new view transition

---
+++ RWGame/AboutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutViewController.swift
+//  RWGame
+//
+//  Created by Samantha Cortopassi on 1/16/19.
+//  Copyright © 2019 Samantha Cortopassi. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
